6444351cf9470d78c0c8d1b645fe888360087681
khalidhu <khalid_h@hotmail.com>
2021-05-14 22:00:30 +0100

    Create Allergies.cs

---
+++ allergies/Allergies.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Allergen
+{
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
+}
+
+public class Allergies
+{
+    private int  allergenValue;
+
+    public int Mask
+    {
+        set => allergenValue = value;
+    }
+    public Allergies(int mask) => Mask = mask;
+    public bool IsAllergicTo(Allergen allergen) => (int)allergen == (allergenValue & (int)allergen);
+
+    public Allergen[] List() => Enum.GetValues(typeof(Allergen)).Cast<Allergen>().Where(IsAllergicTo).ToArray();
+}
+
