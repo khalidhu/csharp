25dabe78e2a5c5b5d7fbcdeacbe352f2a34c56e0
khalidhu <khalid_h@hotmail.com>
2021-05-14 22:07:31 +0100

    Create DifferenceOfSquares.cs

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    private static readonly Func<int, int> SumOfSquares = (x) => (int)Enumerable.Range(1, x - 1 + 1).Select(i => Math.Pow(i, 2)).Sum();
+    private static readonly Func<int, int> SquareOfSum = (x) => Enumerable.Range(1, x - 1 + 1).Select(i => i).Sum();
+
+    public static int CalculateSquareOfSum(int max) => (int)Math.Pow(SquareOfSum(max), 2);
+
+    public static int CalculateSumOfSquares(int max) => SumOfSquares(max); 
+
+    public static int CalculateDifferenceOfSquares(int max) => CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+
+
+}
