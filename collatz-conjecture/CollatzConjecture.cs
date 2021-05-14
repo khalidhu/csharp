c65aceeb18046c28ac011ebb01f0fe871b58d41a
khalidhu <khalid_h@hotmail.com>
2021-05-14 22:07:23 +0100

    Create CollatzConjecture.cs

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static Func<int, int> Sequence = n => (n == 1) ? 0 : n % 2 == 0 ? Sequence(n / 2) + 1 : Sequence(3 * n + 1) + 1;
+    public static int Steps(int number) => (number < 1) ? throw new ArgumentOutOfRangeException() : Sequence(number);
+
+}
