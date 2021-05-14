8c9ced375ec081a273e7c373ec3b36552852c2b9
khalidhu <khalid_h@hotmail.com>
2021-05-14 22:00:23 +0100

    Create Acronym.cs

---
+++ acronym/Acronym.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase) => WordSplit(phrase.Split(new char[] { ' ', '_', '-' }, StringSplitOptions.RemoveEmptyEntries));
+    public static string WordSplit(string phrase) => string.Join("",(phrase.Split(' ').Select(x => x[0]).ToArray())).ToUpper();
+
+}
+
