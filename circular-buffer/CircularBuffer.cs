b90b934d34343e759a503fca7eb64edf28c1d9a2
khalidhu <khalid_h@hotmail.com>
2021-05-14 22:01:10 +0100

    Create CircularBuffer.cs

---
+++ circular-buffer/CircularBuffer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{   
+    private int capacity { get; }
+    private readonly Queue<T> buffer  = new Queue<T>();
+
+    public CircularBuffer(int capacity) => this.capacity = capacity;
+    public T Read() => buffer.Count == 0 ? throw new InvalidOperationException("Cannot read from empty buffer") : buffer.Dequeue();
+    
+    public void Write(T value)
+    {
+        if(buffer.Count == capacity)
+        {
+            throw new InvalidOperationException("Cannot write to full buffer");
+        }
+        buffer.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+      
+        if (buffer.Count == capacity)
+        {
+            buffer.Dequeue();
+        }
+
+        buffer.Enqueue(value);
+    }
+
+    public void Clear() => buffer.Clear();
+}
