a3d6b742b2ab01c4a690a846c6db9e9b993e42db
khalidhu <khalid_h@hotmail.com>
2021-05-14 22:00:42 +0100

    Create ArmstrongNumbers.cs

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+   
+    private static int ArmstrongNumberSum(int number)
+    {
+        string text = number.ToString();
+        return (int)Enumerable.Range(0, text.Length).Select(i => Math.Pow(char.GetNumericValue(text[i]), text.Length)).Sum();
+    }
+
+    public static bool IsArmstrongNumber(int number) => ArmstrongNumberSum(number) == number;
+
+
+}
