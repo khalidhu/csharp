1c609b1826dd7600196dd26efdda0bd04da59a96
khalidhu <khalid_h@hotmail.com>
2021-05-17 08:39:55 +0100

    Create Isogram.cs

---
+++ isogram/Isogram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Isogram
+{
+
+    public static bool IsIsogram(string word) => RemovedUnwantedChars(word.ToLower()).Length == GetDistinctValues(word.ToLower()).Length;
+    public static string GetDistinctValues(string word) => RemovedUnwantedChars(string.Join("", word.Distinct()));
+    public static string RemovedUnwantedChars(string word) => Regex.Replace(word, "[^a-z]", "");
+}
+ 
